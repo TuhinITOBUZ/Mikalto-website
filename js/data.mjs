9a9af8826b3a015d54e03e94fc5e42885c768bdd
Tuhin Mukherjee <tuhin@itobuz.com>
2023-03-27 17:41:54 +0530

    backend update

--- js/data.mjs
+++ js/data.mjs
@@ -1,7 +1,11 @@
 export const pageBanner = "https://i.ibb.co/7jdCPws/banner-Banner.jpg";
+
 export const homestay1 = "https://i.ibb.co/K7khbYz/homestay1.jpg";
+
 export const homestay2 = "https://i.ibb.co/GknZN9C/homestay2.jpg";
+
 export const homestay3 = "https://i.ibb.co/y5cGBhb/homestay3.jpg";
+
 export const wellnessService = {
   url: "https://i.ibb.co/xYCYXrH/wellness.jpg",
   heading: "WELLNESS",
@@ -9,6 +13,7 @@ export const wellnessService = {
   details:
     " Aman's approach to wellness is focused on personalisation , privacy and seclusion. Local spa rituals with indigenous ingredients, offer a profoundly therapeutic touch, and a gentle sense of discovery.",
 };
+
 export const giftCardService = {
   url: "https://i.ibb.co/2NwHZBM/giftCard.jpg",
   heading: "GIFT CARDS",
@@ -34,22 +39,37 @@ export const adventureService = {
 };
 
 export const showRooms = {
-    standardRoom : {
-        url : "https://i.ibb.co/WNrz0C5/standard-Room.jpg",
-        heading : "STANDARD ROOM",
-        price : "From: $150 / night",
-        details : "With wide and comfortable areas and daylight pouring in from the ... "
-    },
-    dulexRoom : {
-        url : "https://i.ibb.co/fG1M7XP/dulex-Room.jpg",
-        heading : "DULEX ROOM",
-        price : "From: $200 / night",
-        details : "With wide and comfortable areas and daylight pouring in from the ... "
-    },
-    superDulexRoom : {
-        url : "https://i.ibb.co/p12W1wV/super-Dulex-Room.jpg",
-        heading : "SUPER DULEX ROOM",
-        price : "From: $300 / night",
-        details : "With wide and comfortable areas and daylight pouring in from the ... "
-    }
-}
+  standardRoom: {
+    url: "https://i.ibb.co/WNrz0C5/standard-Room.jpg",
+    heading: "STANDARD ROOM",
+    price: "From: $150 / night",
+    details:
+      "With wide and comfortable areas and daylight pouring in from the ... ",
+    bedSize: "Queen Size Bed",
+    capacity: "2 adults, 1 childeren",
+    roomSize: "55 sqm",
+    hotelView: "garden view",
+  },
+  dulexRoom: {
+    url: "https://i.ibb.co/fG1M7XP/dulex-Room.jpg",
+    heading: "DELUXE ROOM",
+    price: "From: $200 / night",
+    details:
+      "With wide and comfortable areas and daylight pouring in from the ... ",
+    bedSize: "King Size Bed",
+    capacity: "2 adults, 2 childeren",
+    roomSize: "75 sqm",
+    hotelView: "city view",
+  },
+  superDulexRoom: {
+    url: "https://i.ibb.co/p12W1wV/super-Dulex-Room.jpg",
+    heading: "SUPER DELUXE ROOM",
+    price: "From: $300 / night",
+    details:
+      "With wide and comfortable areas and daylight pouring in from the ... ",
+    bedSize: "King Size Bed",
+    capacity: "3 adults, 2 childeren",
+    roomSize: "90 sqm",
+    hotelView: "garden view",
+  },
+};
--- js/main.js
+++ js/main.js
@@ -2,89 +2,6 @@ const slides = document.querySelectorAll(".slide");
 const form = document.getElementById("page-banner-form");
 let counter = 0;
 
-slides.forEach((slide, index) => {
-  slide.style.left = `${index * 150}%`;
-});
-
-const goPreviousSlide = () => {
-  counter = (counter - 1) % 3;
-  // console.log(counter);
-  slideImage();
-  async function setData() {
-    const response = await fetch(`http://localhost:5000/`)
-    .then(function (res) {
-      return res.json();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-    if (response.error) {
-      alert(response.error.message);
-    } else { 
-      if (counter === 0){
-        document.getElementById("showRoomHeading").textContent = response.showRooms.standardRoom.heading
-        document.getElementById("showRoomPrice").textContent = response.showRooms.standardRoom.price
-        document.getElementById("showRoomDetails").textContent = response.showRooms.standardRoom.details
-      }
-      else if ( counter === 1){
-        document.getElementById("showRoomHeading").textContent = response.showRooms.dulexRoom.heading
-        document.getElementById("showRoomPrice").textContent = response.showRooms.dulexRoom.price
-        document.getElementById("showRoomDetails").textContent = response.showRooms.dulexRoom.details
-
-      }
-      else if (counter === 2){
-        document.getElementById("showRoomHeading").textContent = response.showRooms.superDulexRoom.heading
-        document.getElementById("showRoomPrice").textContent = response.showRooms.superDulexRoom.price
-        document.getElementById("showRoomDetails").textContent = response.showRooms.superDulexRoom.details
-
-      }
-    }
-  }
-  setData()
-};
-const goNextSlide = () => {
-  counter = (counter + 1) % 3;
-  // console.log(counter);
-  slideImage();
-  async function setData() {
-    const response = await fetch(`http://localhost:5000/`)
-    .then(function (res) {
-      return res.json();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-    if (response.error) {
-      alert(response.error.message);
-    } else { 
-      if (counter === 0){
-        document.getElementById("showRoomHeading").textContent = response.showRooms.standardRoom.heading
-        document.getElementById("showRoomPrice").textContent = response.showRooms.standardRoom.price
-        document.getElementById("showRoomDetails").textContent = response.showRooms.standardRoom.details
-      }
-      else if ( counter === 1){
-        document.getElementById("showRoomHeading").textContent = response.showRooms.dulexRoom.heading
-        document.getElementById("showRoomPrice").textContent = response.showRooms.dulexRoom.price
-        document.getElementById("showRoomDetails").textContent = response.showRooms.dulexRoom.details
-
-      }
-      else if (counter === 2){
-        document.getElementById("showRoomHeading").textContent = response.showRooms.superDulexRoom.heading
-        document.getElementById("showRoomPrice").textContent = response.showRooms.superDulexRoom.price
-        document.getElementById("showRoomDetails").textContent = response.showRooms.superDulexRoom.details
-
-      }
-    }
-  }
-  setData()
-};
-
-const slideImage = () => {
-  slides.forEach((slide) => {
-    slide.style.transform = `translateX(-${counter * 150}%)`;
-  });
-};
-
 async function getData() {
   const response = await fetch(`http://localhost:5000/`)
     .then(function (res) {
@@ -145,6 +62,90 @@ async function getData() {
   }
 }
 getData();
+
+async function setData() {
+  const response = await fetch(`http://localhost:5000/`)
+    .then(function (res) {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  if (response.error) {
+    alert(response.error.message);
+  } else {
+    if (counter === 0) {
+      document.getElementById("showRoomHeading").textContent =
+        response.showRooms.standardRoom.heading;
+      document.getElementById("showRoomPrice").textContent =
+        response.showRooms.standardRoom.price;
+      document.getElementById("showRoomDetails").textContent =
+        response.showRooms.standardRoom.details;
+      document.getElementById("bed-size").textContent =
+        response.showRooms.standardRoom.bedSize;
+      document.getElementById("capacity").textContent =
+        response.showRooms.standardRoom.capacity;
+      document.getElementById("room-size").textContent =
+        response.showRooms.standardRoom.roomSize;
+      document.getElementById("hotel-view").textContent =
+        response.showRooms.standardRoom.hotelView;
+    } else if (counter === 1) {
+      document.getElementById("showRoomHeading").textContent =
+        response.showRooms.dulexRoom.heading;
+      document.getElementById("showRoomPrice").textContent =
+        response.showRooms.dulexRoom.price;
+      document.getElementById("showRoomDetails").textContent =
+        response.showRooms.dulexRoom.details;
+      document.getElementById("bed-size").textContent =
+        response.showRooms.dulexRoom.bedSize;
+      document.getElementById("capacity").textContent =
+        response.showRooms.dulexRoom.capacity;
+      document.getElementById("room-size").textContent =
+        response.showRooms.dulexRoom.roomSize;
+      document.getElementById("hotel-view").textContent =
+        response.showRooms.dulexRoom.hotelView;
+    } else if (counter === 2) {
+      document.getElementById("showRoomHeading").textContent =
+        response.showRooms.superDulexRoom.heading;
+      document.getElementById("showRoomPrice").textContent =
+        response.showRooms.superDulexRoom.price;
+      document.getElementById("showRoomDetails").textContent =
+        response.showRooms.superDulexRoom.details;
+      document.getElementById("bed-size").textContent =
+        response.showRooms.superDulexRoom.bedSize;
+      document.getElementById("capacity").textContent =
+        response.showRooms.superDulexRoom.capacity;
+      document.getElementById("room-size").textContent =
+        response.showRooms.superDulexRoom.roomSize;
+      document.getElementById("hotel-view").textContent =
+        response.showRooms.superDulexRoom.hotelView;
+    }
+  }
+}
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 150}%`;
+});
+
+const goPreviousSlide = () => {
+  counter = (counter - 1) % 3;
+  if (counter < 0) {
+    counter = 0;
+  }
+  slideImage();
+  setData();
+};
+const goNextSlide = () => {
+  counter = (counter + 1) % 3;
+  slideImage();
+  setData();
+};
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 150}%)`;
+  });
+};
 
 function handleFormSubmit() {
   let checkIn = document.getElementById("check-in").value;
